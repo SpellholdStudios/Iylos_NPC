b030daa3670c4114281ee705be1d7592810bdaed
Gitjas <Gitjas@users.noreply.github.com>
2020-05-15 20:41:52 +0200

    Fixed Ascension crossmod for ascension 2.08 and higher.

--- iylos/dialogue/crossmod/ascension.d
+++ iylos/dialogue/crossmod/ascension.d
@@ -1,15 +1,19 @@
 // -------------------------------------
-// Crossmod content - Ascension
+// Crossmod content - Ascension: "Ascension main component: rewritten Chapter 10"
 // -------------------------------------
 // Jon Irenicus - Ascension
-I_C_T IRENIC2 12 LK#IylosJonIrenicusComment
+/* ~So my ungrateful sister, you finally stand by my side. I shall deal with your treachery afterwards. As for you, <CHARNAME>, Melissan has granted me the power to rip the divinity from your body. The first thing I shall feel in an eon shall be pleasure as I watch your corpse turn to dust.~ */
+
+I_C_T IRENIC2 %iylos_irenic2% LK#IylosJonIrenicusComment
   == LK#IYLJ IF ~InParty("Iylos") InMyArea("Iylos") !StateCheck("Iylos",CD_STATE_NOTVALID)~ THEN @0
   == IRENIC2 @1
   == IRENIC2 @2
 END
 
 // Balthazar - Ascension
-I_C_T BALTH2 5 LK#IylosBalth2Comment
+/* ~She has, no doubt, guarded the pools in some fashion... but it is the only way to continue that I can think of. Short of standing here and watching her become a goddess, that is.~ */
+
+I_C_T BALTH2 %iylos_balth2_1% LK#IylosBalth2Comment
   == LK#IYLJ IF ~InParty("Iylos") InMyArea("Iylos") !StateCheck("Iylos",CD_STATE_NOTVALID)~ THEN @3
   == BALTH2  @4
   == LK#IYLJ @5
@@ -18,10 +22,3 @@ I_C_T BALTH2 5 LK#IylosBalth2Comment
 END
 
 
-// Balthazar - Ascension
-I_C_T BALTH2 9 LK#IylosBalth2Comment2
-  == LK#IYLJ IF ~InParty("Iylos") InMyArea("Iylos") !StateCheck("Iylos",CD_STATE_NOTVALID)~ THEN @8
-  == BALTH2  @9
-  == LK#IYLJ @10
-  == BALTH2  @11
-END
---
+++ iylos/dialogue/crossmod/ascension_10.d
@@ -0,0 +1,12 @@
+// -------------------------------------
+// Crossmod content - Ascension: "Redeemable Balthazar"
+// -------------------------------------
+// Balthazar - Ascension
+/* ~And there is nothing that I ever wanted more. I choose to become a mortal gladly, and with all my heart. To walk the earth without that dark burden twisting my soul... I... I cannot even imagine...~ */
+
+I_C_T BALTH2 %iylos_balth2_2% LK#IylosBalth2Comment2
+  == LK#IYLJ IF ~InParty("Iylos") InMyArea("Iylos") !StateCheck("Iylos",CD_STATE_NOTVALID)~ THEN @8
+  == BALTH2  @9
+  == LK#IYLJ @10
+  == BALTH2  @11
+END
